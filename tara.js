4604d19412296e09fd8344ba4619e74d57876910
TaraMKelly <tara.kelly16@gmail.com>
2021-07-13 12:11:38 -0400

    added review form to cards, started working on user ratings with stars

---
+++ tara.js
@@ -0,0 +1,77 @@
+
+const form = document.querySelector('#search-form')
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let query = e.target.search.value
+
+    fetchData(query)
+})
+
+function fetchData(query){
+    fetch('http://localhost:3000/results')
+    .then(res => res.json())
+    .then(data => data.forEach(renderMealsCard))
+}
+function rating(){
+    return (
+        `<div class="rate">
+        <input type="radio" id="star5" name="rate" value="5" />
+        <label for="star5" title="text">5 stars</label>
+        <input type="radio" id="star4" name="rate" value="4" />
+        <label for="star4" title="text">4 stars</label>
+        <input type="radio" id="star3" name="rate" value="3" />
+        <label for="star3" title="text">3 stars</label>
+        <input type="radio" id="star2" name="rate" value="2" />
+        <label for="star2" title="text">2 stars</label>
+        <input type="radio" id="star1" name="rate" value="1" />
+        <label for="star1" title="text">1 star</label>
+    </div>`
+    )
+}
+function renderMealsCard(mealInfo){
+    let divFrame = document.createElement('div')
+    let title = document.createElement('h2')
+    let img = document.createElement('img')
+    title.textContent = mealInfo.title
+    img.src = mealInfo.image
+
+    let ratingStars = document.createElement('p')
+    ratingStars.innerHTML = rating()
+
+    let avgRating = 0
+    let pAvgRating = document.createElement("p")
+    pAvgRating.innerText = `User Rating: ${avgRating}`
+
+    // ratingStars.addEventListener("click", (e) => {
+    //     if (e.target.rate.value === 5) 
+    //     if (e.target.rate.value === 4)
+    //     if (e.target.rate.value === 3)
+    //     if (e.target.rate.value === 2)
+    //     if (e.target.rate.value === 1)
+    // })
+
+    let container = document.querySelector('#search-results')
+    container.append(divFrame)
+
+    let reviewForm = document.createElement("form");
+    reviewForm.setAttribute("class", "review_form")
+    let review = document.createElement("input");
+    review.setAttribute("type", "text");
+    review.setAttribute("name", "review");
+    review.setAttribute("placeholder", "Leave a review...");
+    let reviewButton = document.createElement("button");
+    reviewButton.setAttribute("type", "submit");
+    reviewButton.textContent = "Submit"
+    reviewForm.append(review, reviewButton)
+
+    let ul = document.createElement("ul")
+    reviewForm.addEventListener("submit", (e) => {
+        e.preventDefault()
+        let newReview = document.createElement("li")
+        newReview.textContent = e.target.review.value
+        ul.append(newReview)
+        e.target.review.value = ""
+    })
+    reviewForm.append(ul)
+    divFrame.append(title, img, ratingStars, pAvgRating, reviewForm)
+}

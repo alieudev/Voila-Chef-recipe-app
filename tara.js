39e1bb48dcb5f24fc093dbf26409b5069b301302
TaraMKelly <tara.kelly16@gmail.com>
2021-07-14 11:37:58 -0400

    rearraged my code to make it easier to read as i debug the ratings section

--- tara.js
+++ tara.js
@@ -34,27 +34,18 @@ function renderMealsCard(mealInfo){
     let img = document.createElement('img')
     title.textContent = mealInfo.title
     img.src = mealInfo.image
+    //user ratings
+    let avgRating = mealInfo.userRating
+    console.log(avgRating)
+    // let pRating = document.querySelector('p')
+    // pRating.textContent = `User Rating: ${avgRating}`
+    // console.log(pRating)
 
+    //ratings stars
     let ratingStars = document.createElement('p')
     ratingStars.innerHTML = rating()
-
-    let pRating = document.querySelector("p")
-    let totalRatings = mealInfo.totalRatings
-    let avgRating = mealInfo.userRating
-    pRating.textContent = `User Rating: ${avgRating}`
-
-    ratingStars.addEventListener("click", (e) => {
-        console.log(e.target)
-        if (e.target.value === "5") return avgRating = (avgRating +=5)/(totalRatings++)
-        if (e.target.value === "4") return avgRating = (avgRating +=4)/(totalRatings++)
-        if (e.target.value === "3") return avgRating = (avgRating +=3)/(totalRatings++)
-        if (e.target.value === "2") return avgRating = (avgRating +=2)/(totalRatings++)
-        if (e.target.value === "1") return avgRating = (avgRating +=1)/(totalRatings++)
-    })
-
-    let container = document.querySelector('#search-results')
-    container.append(divFrame)
-
+    console.log(ratingStars)
+    //review form
     let reviewForm = document.createElement("form");
     reviewForm.setAttribute("class", "review_form")
     let review = document.createElement("input");
@@ -65,7 +56,7 @@ function renderMealsCard(mealInfo){
     reviewButton.setAttribute("type", "submit");
     reviewButton.textContent = "Submit"
     reviewForm.append(review, reviewButton)
-
+    //new review event listener
     let ul = document.createElement("ul")
     reviewForm.addEventListener("submit", (e) => {
         e.preventDefault()
@@ -74,6 +65,21 @@ function renderMealsCard(mealInfo){
         ul.append(newReview)
         e.target.review.value = ""
     })
+    let container = document.querySelector('#search-results')
+    container.append(divFrame)
     reviewForm.append(ul)
-    divFrame.append(title, img, ratingStars, pRating, reviewForm)
+    divFrame.append(title, img, ratingStars, reviewForm)
+
+
+
+    //attempt to make star ratings do something functional
+    // ratingStars.addEventListener("click", (e) => {
+    //     console.log(e.target)
+        // let totalRatings = mealInfo.totalRatings
+    //     if (e.target.value === "5") return avgRating = (avgRating +=5)/(totalRatings++)
+    //     if (e.target.value === "4") return avgRating = (avgRating +=4)/(totalRatings++)
+    //     if (e.target.value === "3") return avgRating = (avgRating +=3)/(totalRatings++)
+    //     if (e.target.value === "2") return avgRating = (avgRating +=2)/(totalRatings++)
+    //     if (e.target.value === "1") return avgRating = (avgRating +=1)/(totalRatings++)
+    // })
 }
